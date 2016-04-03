5a98bb709ff82b4a8007ef7bf93f91faf3438640
Songhua <xsonghua@gmail.com>
2016-04-03 02:50:20 -0700

    first commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("hello world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
